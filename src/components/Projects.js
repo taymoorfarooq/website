3e92ce91e09024cb3f3ba42e2d053a13d5cb06f7
taymoorfarooq <taymoor.farooq@mail.utoronto.ca>
2023-04-03 00:33:51 -0400

    margin fixes, masthead update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,21 +7,31 @@ export default function Projects() {
 
   return(
     <div>
-      <img id="mh" src="1824.gif"></img>
-    {/* <p id='mastheadtext'>Hi! I'm Taymoor.</p> */}
+      <img id="masthead-header" src="whitebg.gif" onContextMenu={(e) => e.preventDefault()}></img>
+      
+      <img id="masthead-arrow-animation" src="masthead-arrow-animation.gif" onContextMenu={(e) => e.preventDefault()}
+          onMouseOver={e => (e.currentTarget.src = "altscrolltext.png")}
+          onMouseOut={e => (e.currentTarget.src = "masthead-arrow-animation.gif")}></img>
 
     <Link id='cover1' to='/voyageur'><img src='img2.png' alt='voyageur cover'
-          onMouseOver={e => (e.currentTarget.src = "img1.png")}
+          onMouseOver={e => (e.currentTarget.src = "img2-hover.png")}
           onMouseOut={e => (e.currentTarget.src = "img2.png")}></img></Link>
     <Link id='cover2' to='/'><img src='img1.png' alt='thing'
-          onMouseOver={e => (e.currentTarget.src = "img2.png")}
+          onMouseOver={e => (e.currentTarget.src = "img1-hover.png")}
           onMouseOut={e => (e.currentTarget.src = "img1.png")}></img></Link>
     <Link id='cover3' to='/'><img src='img3.png' alt='thing'
-          onMouseOver={e => (e.currentTarget.src = "img1.png")}
+          onMouseOver={e => (e.currentTarget.src = "img3-hover.png")}
           onMouseOut={e => (e.currentTarget.src = "img3.png")}></img></Link>
     <Link id='cover4' to='/'><img src='img4.png' alt='thing'
-          onMouseOver={e => (e.currentTarget.src = "img1.png")}
+          onMouseOver={e => (e.currentTarget.src = "img4-hover.png")}
           onMouseOut={e => (e.currentTarget.src = "img4.png")}></img></Link>
+    <Link id='cover5' to='/'><img src='img5.png' alt='thing'
+          onMouseOver={e => (e.currentTarget.src = "img5-hover.png")}
+          onMouseOut={e => (e.currentTarget.src = "img5.png")}></img></Link>
+  <Link id='cover6' to='/'><img src='img6.png' alt='thing'
+          onMouseOver={e => (e.currentTarget.src = "img6-hover.png")}
+          onMouseOut={e => (e.currentTarget.src = "img6.png")}></img></Link>
+
     </div>
 
   )
