b773c79aa93a705f4b1e259a1b1d0a5595d15d11
taymoorfarooq <taymoor.farooq@mail.utoronto.ca>
2023-04-07 05:59:02 -0400

    fix code placement

--- src/App.js
+++ src/App.js
@@ -1,81 +1,68 @@
-import {Route, Routes} from "react-router-dom"
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Masthead from "./components/Masthead";
-import Contact from "./components/Contact";
-
-import './styles/App.css'
-
-let innerCursor = document.querySelector(".inner-cursor");
-let outerCursor = document.querySelector(".outer-cursor");
-
-document.addEventListener("mousemove", moveCursor);
-
-function moveCursor(e){
-    let x = e.clientX;
-    let y = e.clientY;
-
-    innerCursor.style.left = `${x}px`;
-    innerCursor.style.top = `${y}px`;
-    outerCursor.style.left = `${x}px`;
-    outerCursor.style.top = `${y}px`;
-}
-
-let links = Array.from(document.querySelectorAll("a"));
-
-console.log(links);
-
-links.forEach(link => {
-    link.addEventListener("mouseover", () => {
-        innerCursor.classList.add("grow");
-        outerCursor.classList.add("grow");
-    });
-});
-
-links.forEach(link => {
-    link.addEventListener("mouseleave", () => {
-        innerCursor.classList.remove("grow");
-        outerCursor.classList.remove("grow");
-    });
-});
-
+import Projects from './components/Projects';
+import Masthead from './components/Masthead';
+import Contact from './components/Contact';
+import './styles/App.css';
 
 function App() {
+  useEffect(() => {
+    let innerCursor = document.querySelector('.inner-cursor');
+    let outerCursor = document.querySelector('.outer-cursor');
 
-    
+    document.addEventListener('mousemove', moveCursor);
+
+    function moveCursor(e) {
+      let x = e.clientX;
+      let y = e.clientY;
+
+      innerCursor.style.left = `${x}px`;
+      innerCursor.style.top = `${y}px`;
+      outerCursor.style.left = `${x}px`;
+      outerCursor.style.top = `${y}px`;
+    }
+
+    let links = Array.from(document.querySelectorAll('a'));
+
+    links.forEach((link) => {
+      link.addEventListener('mouseover', () => {
+        innerCursor.classList.add('grow');
+        outerCursor.classList.add('grow');
+      });
+    });
+
+    links.forEach((link) => {
+      link.addEventListener('mouseleave', () => {
+        innerCursor.classList.remove('grow');
+        outerCursor.classList.remove('grow');
+      });
+    });
+
+    return () => {
+      document.removeEventListener('mousemove', moveCursor);
+    };
+  }, []);
+
   return (
-      <>
-
+    <>
       <div className="container">
-        
-
         <Navbar />
-
-        <div id="mdiv" class="panels">
-            <Masthead />
+        <div id="mdiv" className="panels">
+          <Masthead />
         </div>
-
-        <div id="pdiv" class="panels">
-
-            <Routes>
-                <Route path="/" element={<Projects/>}/>
-                <Route path="*" element={<Projects />} />
-            </Routes>
+        <div id="pdiv" className="panels">
+          <Routes>
+            <Route path="/" element={<Projects />} />
+            <Route path="*" element={<Projects />} />
+          </Routes>
         </div>
-
-        <div id="cdiv" class="panels">
-        <Contact />   
+        <div id="cdiv" className="panels">
+          <Contact />
         </div>
-       
-     
       </div>
-      {/* <ScrollUp />
-      <SocialIcons /> */}
-
-      </>
-      
-  )
+    </>
+  );
 }
 
 export default App;
--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -1,48 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import '../styles/Masthead.css';
 
-var masthead = document.getElementById('masthead');
-
-function fadeOutOnScroll(element) {
-	if (!element) {
-		return;
-	}
-	
-	var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-	var elementHeight = element.offsetHeight;
-	var scrollTop = document.documentElement.scrollTop;
-	
-	var opacity = 1;
-	
-	if (scrollTop > distanceToTop) {
-		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-	}
-	
-	if (opacity >= 0) {
-		element.style.opacity = opacity;
-	}
-}
-
-function scrollHandler() {
-	fadeOutOnScroll(masthead);
-}
-
-window.addEventListener('scroll', scrollHandler);
-
 export default function Masthead() {
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    function fadeOutOnScroll() {
+      const element = document.getElementById('masthead');
+
+      if (!element) {
+        return;
+      }
+
+      const distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+      const elementHeight = element.offsetHeight;
+      const scrollTop = document.documentElement.scrollTop;
+
+      const newOpacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+      setOpacity(newOpacity >= 0 ? newOpacity : 0);
+    }
+
+    window.addEventListener('scroll', fadeOutOnScroll);
+
+    return () => {
+      window.removeEventListener('scroll', fadeOutOnScroll);
+    };
+  }, []);
+
   return (
-    
-    
-    <div id="masthead">
+    <div id="masthead" style={{ opacity }}>
       <h1 id="mastheader">TAYMOOR</h1>
-      <p id="mastheadparagraph">I'm a Toronto-based artist studying Computational Cognition at the University of Toronto</p>
+      <p id="mastheadparagraph">
+        I'm a Toronto-based artist studying Computational Cognition at the University of Toronto
+      </p>
       <div id="button">
-            <a href='youtube.com'><p>SAY HI!</p></a>
-          
+        <a href="youtube.com">
+          <p>SAY HI!</p>
+        </a>
       </div>
-      </div>
-      
-  )
-  
-  
+    </div>
+  );
 }
